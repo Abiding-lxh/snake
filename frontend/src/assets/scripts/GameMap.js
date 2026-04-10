ee5574103e261396219b1a92f9ed00b6d59e0ae5
AbidingX <abidinglxh@outlook.com>
2026-04-10 17:21:12 +0800

    add snake

--- frontend/src/assets/scripts/GameMap.js
+++ frontend/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { SnakeGameObject } from './SnakeGameObject'
 import { Wall } from './Wall'
+import { Snake } from "./Snake"
 
 export class GameMap extends SnakeGameObject{
 	constructor(ctx,parent){
@@ -13,6 +14,12 @@ export class GameMap extends SnakeGameObject{
 
 		this.inner_walls_counts=40;
 		this.walls=[];
+
+		this.snakes=[
+			new Snake({id:0,color:"#4876EC",r:this.rows-2,c:1},this),
+			new Snake({id:1,color:"#F94848",r:1,c:this.cols-2},this)
+		]
+
 	}
 	check_connectivity(g,sx,sy,tx,ty){
 		if(sx==tx&&sy==ty)return true;
@@ -68,19 +75,69 @@ export class GameMap extends SnakeGameObject{
 		}
 		return true
 	}
+	add_listening_events(){
+		this.ctx.canvas.focus();
+		const [snake0,snake1]=this.snakes;
+		this.ctx.canvas.addEventListener("keydown",e=>{
+			if(e.key==='w')snake0.set_direction(0);
+			else if(e.key==='d')snake0.set_direction(1);
+			else if(e.key==='s')snake0.set_direction(2);
+			else if(e.key==='a')snake0.set_direction(3);
+			else if(e.key==='ArrowUp')snake1.set_direction(0);
+			else if(e.key==='ArrowRight')snake1.set_direction(1);
+			else if(e.key==='ArrowDown')snake1.set_direction(2);
+			else if(e.key==='ArrowLeft')snake1.set_direction(3);
+		})
+	}
 	start(){
 		for(let i=0;i<1000;i++){
 			if(this.create_walls())
 				break;
 		}
+		this.add_listening_events();
 	}
 	update_size(){
 		this.L=parseInt(Math.min(this.parent.clientWidth/this.cols,this.parent.clientHeight/this.rows));
 		this.ctx.canvas.width=this.L*this.cols;
 		this.ctx.canvas.height=this.L*this.rows;
 	}
+
+	check_ready(){	//判断两条蛇是否准备好下一回合
+		for (const snake of this.snakes){
+			if(snake.status!=="idle")return false;
+			if(snake.direction===-1)return false;
+		}
+		return true;
+	}
+	next_step(){
+		for(const snake of this.snakes){
+			snake.next_step();
+		}
+	}
+	check_valid(cell){	//检测是否碰撞，是否合法
+		for(const wall of this.walls){
+			if(wall.r===cell.r&&wall.c===cell.c){
+				return false;
+			}
+		}
+		for(const snake of this.snakes){
+			let k=snake.cells.length;
+			if(!snake.check_tail_increasing()){
+				k--;
+			}
+			for(let i=0;i<k;i++){
+				if(snake.cells[i].r===cell.r&&snake.cells[i].c===cell.c){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 	update(){
 		this.update_size();
+		if(this.check_ready()){
+			this.next_step();
+		}
 		this.render();
 	}
 	render(){
